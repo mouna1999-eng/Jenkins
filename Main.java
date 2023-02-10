db25fe7c4938b4475ce245aa0ca075c69460e242
Mouna JARRAY <57876552+mouna1999-eng@users.noreply.github.com>
2023-02-10 13:56:52 +0100

    Update Main.java
    ok

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main { 
    public static void main(String[] args) {
-      System.out.println( "Hello, Mouna JARRAY!");
+      System.out.println( "Hello, Mouna JARRAY 2!");
    }
 
 }
